2281e137b3495e7723f3e25201e851c8a1c46a72
Osama Mishmish <osama.mishmish16@gmail.com>
2022-08-17 02:21:57 +0200

    prepare files of the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import First from './Components/First';
+import Second from './Components/Second';
 
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <First/>
+        <Second/>
+      </div>
+    )
+  }
+}
 export default App;
---
+++ src/Components/Second.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Second extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default Second;
