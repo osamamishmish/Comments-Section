cea1c5b5276776182a119665cbe07a6f410f2858
Osama Mishmish <osama.mishmish16@gmail.com>
2022-08-27 18:00:55 +0200

    add user reply on second comment

--- src/Components/Second.js
+++ src/Components/Second.js
@@ -3,6 +3,8 @@ import max from "../Images/image-maxblagun.png";
 import julio from "../Images/image-juliusomo.png";
 import "./Second.css";
 import $ from "jquery";
+
+
 class Second extends Component {
   state={
     user:[
@@ -14,23 +16,49 @@ class Second extends Component {
       }
     ],
     Reply:[{
-      comment:""
-    }
       
-    ]
+    }]
   }
-  ShowReply=()=>{
+  Reply=[];
+  ShowReplyField=()=>{
     $(".reply-container").addClass("show")
+    $(".reply-container textarea").text("@maxblagun,")
     }
-  
+  SaveComment=(e)=>{
+this.Reply.comment=e.target.value
+this.Reply.id=this.Reply.length
+
+this.setState({
+  Reply:this.Reply
+})
+
+  }
+  AddReply=()=>{
+    const container=document.createElement("div"),
+          img=document.createElement("img"),
+          headerContainer=document.createElement("div"),
+          buttonsContainer=document.createElement("div"),
+          Delete=document.createElement("button"),
+          edit=document.createElement("button"),
+          text=document.createElement("p");
+          $(Delete).text("Delete").appendTo(buttonsContainer);
+          $(edit).text("Edit").appendTo(buttonsContainer);
+          $(buttonsContainer).appendTo(headerContainer).addClass("buttons-container");
+          $(headerContainer).addClass("header-container").prependTo(container);
+          $(img).attr("src",julio).prependTo(headerContainer);
+          $(text).text(this.state.Reply.comment).appendTo(container);
+          $(container).addClass(this.state.Reply.id).attr("id","replies").insertAfter(".second-comment")
+  }
   render() {
+    
+    
     const data=this.state.user.map((e)=>{
       return(
         <header className='header' key={e.id}>
       <img src={max} alt=""/>
       <p>{e.name}</p>
       <span className='status'>{e.status}</span>
-      <span className='reply' onClick={this.ShowReply} >Reply</span>
+      <span className='reply' onClick={this.ShowReplyField} >Reply</span>
       
       
       </header>
@@ -52,8 +80,8 @@ class Second extends Component {
       </div>
       <div  className="reply-container">
       <img src={julio} alt=""/>
-        <textarea />
-      <button className="reply-button">Reply</button>
+        <textarea onChange={this.SaveComment}/>
+      <button className="reply-button" onClick={this.AddReply}>Reply</button>
         </div>
         </div>
     )
